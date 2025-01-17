2c059e483009f14345bfa8716684c88c5eedadfb
lubasinkal <lubasi@campempirica.com>
2025-01-17 17:59:26 +0200

    html lsp

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@
 ========         ||:Tutor              ||   |:::::|          ========
 ========         |'-..................-'|   |____o|          ========
 ========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+========        /::::::::::|  |::::::::::\  \ no mouse \     =======(=)
 ========       /:::========|  |==hjkl==:::\  \ required \    ========
 ========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
 ========                                                     ========
@@ -634,6 +634,7 @@ require('lazy').setup({
         pyright = {},
         rust_analyzer = {},
         r_language_server = {},
+        html = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
