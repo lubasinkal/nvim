5e78e4bbe78147d711a8333badb5436a8d63bed7
lubasinkal <lubasinkal@outlook.com>
2025-01-24 09:15:27 +0200

    Switch colourscheme
    Back to Oxocarbon

--- init.lua
+++ init.lua
@@ -870,7 +870,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'oxocarbon'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
