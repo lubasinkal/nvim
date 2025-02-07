d499f5b4d42a9b2a01e4c21d7f55851decdd8a84
lubasinkal <lubasinkal@outlook.com>
2025-02-07 19:51:53 +0200

    r

--- lua/custom/plugins/r-nvim.lua
+++ lua/custom/plugins/r-nvim.lua
@@ -14,12 +14,44 @@
 
 -- TODO: Uncomment only if you have the dependencies setup. :wq then open nvim R.nvim should then build automatically
 
+
+
 return {
-  'R-nvim/R.nvim',
-  -- Only required if you also set defaults.lazy = true
-  lazy = true,
-  -- R.nvim is still young and we may make some breaking changes from time
-  -- to time. For now we recommend pinning to the latest minor version
-  -- like so:
-  -- version = '~0.1.0',
+  -- R.nvim plugin
+  {
+    "R-nvim/R.nvim",
+    -- Only required if you also set defaults.lazy = true
+    lazy = false,
+    -- R.nvim is still young and we may make some breaking changes from time
+    -- to time. For now we recommend pinning to the latest minor version
+    -- like so:
+    version = "~0.1.0"
+  },
+
+  -- cmp-r and nvim-cmp setup
+  {
+    "R-nvim/cmp-r",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      require("cmp").setup({
+        sources = {
+          { name = "cmp_r" }
+        }
+      })
+      require("cmp_r").setup({})
+    end,
+  },
+
+  -- nvim-treesitter configuration
+  {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex", "csv" },
+        highlight = { enable = true },
+      })
+    end,
+  },
 }
+
