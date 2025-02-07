3a1dba59b55e3d44825068922ab215b23f5de7f0
lubasinkal <lubasinkal@outlook.com>
2025-02-07 18:58:03 +0200

    R-nvim install once dependencies are met

--- lua/custom/plugins/r-nvim.lua
+++ lua/custom/plugins/r-nvim.lua
@@ -1,3 +1,19 @@
+
+-- Rtools may be an issue for people without it
+--
+-- FIX: DEPENDENCIES
+--
+-- Make and C compiler
+--       GNU make and a C compiler (e.g. gcc or clang) must be installed to
+--       build the the R package nvimcom which is included in R.nvim and is
+--       automatically installed and updated whenever necessary.
+--
+-- TODO: On Windows, you have to install Rtools to be able to build nvimcom:
+--       https://cran.r-project.org/bin/windows/Rtools/
+--
+
+-- TODO: Uncomment only if you have the dependencies setup. :wq then open nvim R.nvim should then build automatically
+
 return {
   'R-nvim/R.nvim',
   -- Only required if you also set defaults.lazy = true
