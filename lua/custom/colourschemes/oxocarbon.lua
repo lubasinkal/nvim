a47b09aefb334316e292d871ab332ca224b508d5
lubasinkal <lubasinkal@outlook.com>
2025-02-24 22:44:34 +0200

    theme change

--- lua/custom/colourschemes/oxocarbon.lua
+++ lua/custom/colourschemes/oxocarbon.lua
@@ -1,11 +1,11 @@
 return {
   {
     'nyoom-engineering/oxocarbon.nvim',
-    name = 'oxocarbon',
-    lazy = false, -- Load immediately
-    priority = 1000,
-    config = function()
-      vim.cmd 'colorscheme oxocarbon' -- Apply colorscheme
-    end,
+    -- name = 'oxocarbon',
+    -- lazy = false, -- Load immediately
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd 'colorscheme oxocarbon' -- Apply colorscheme
+    -- end,
   },
 }
---
+++ lua/custom/colourschemes/vesper.lua
@@ -0,0 +1,9 @@
+return {
+  'datsfilipe/vesper.nvim',
+  name = 'vesper',
+  lazy = false, -- Load immediately
+  priority = 1000,
+  config = function()
+    vim.cmd 'colorscheme vesper' -- Apply colorscheme
+  end,
+}
