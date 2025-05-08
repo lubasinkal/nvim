79ea89006e1c9ac460c9b355e07f731313df9388
lubasinkal <lubasinkal@outlook.com>
2025-05-08 20:14:48 +0200

    vesper

--- lua/custom/colourschemes/init.lua
+++ lua/custom/colourschemes/init.lua
@@ -3,13 +3,12 @@ return {
   -- name = 'carbonfox',
   -- 'nyoom-engineering/oxocarbon.nvim',
   -- name = 'oxocarbon',
-  'scottmckendry/cyberdream.nvim',
-
+  -- 'scottmckendry/cyberdream.nvim',
+  'datsfilipe/vesper.nvim',
   event = { 'BufRead' },
 
   config = function()
-    vim.cmd 'colorscheme cyberdream' -- Apply colorscheme
-    vim.opt.background = 'dark' -- set this to dark or light
+    vim.cmd 'colorscheme vesper' -- Apply colorscheme
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
