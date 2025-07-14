b21a84b27d2a1e8b873fb72f411f6d1b617f913c
lubasinkal <lubasinkal@outlook.com>
2025-07-14 13:17:22 +0200

    enhance

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -9,5 +9,49 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+    -- Enable inline blame
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      delay = 500, -- Delay before showing blame
+      ignore_whitespace = false,
+    },
+    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+    -- Keymaps
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gs.next_hunk() end)
+        return '<Ignore>'
+      end, { expr = true, desc = 'Next Hunk' })
+
+      map('n', '[c', function()
+        if vim.wo.diff then return '[c' end
+        vim.schedule(function() gs.prev_hunk() end)
+        return '<Ignore>'
+      end, { expr = true, desc = 'Prev Hunk' })
+
+      -- Actions
+      map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = 'Stage Hunk' })
+      map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = 'Reset Hunk' })
+      map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage Buffer' })
+      map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo Stage Hunk' })
+      map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset Buffer' })
+      map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview Hunk' })
+      map('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = 'Blame Line' })
+      map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'Toggle Blame' })
+      map('n', '<leader>hd', gs.diffthis, { desc = 'Diff This' })
+      map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'Diff This ~' })
+      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Select Hunk' })
+    end,
   },
 }
--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -45,9 +45,11 @@ return { -- Useful plugin to show you pending keybinds.
     -- Document existing key chains
     spec = {
       { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d', group = '[D]adbod' },
       { '<leader>r', group = '[R]ename' },
       { '<leader>s', group = '[S]earch' },
+
+      { '<leader>u', group = '[U]I' },
       { '<leader>w', group = '[W]orkspace' },
       { '<leader>t', group = '[T]oggle' },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
