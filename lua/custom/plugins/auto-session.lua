6fa481e7d40fd3eafbe8bb671bbd8f9e9aaad057
lubasinkal <lubasinkal@outlook.com>
2025-07-14 14:43:20 +0200

    Session

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,17 +1,17 @@
 return {
   'rmagatti/auto-session',
-  lazy = false,
+  cmd = { 'SessionRestore', 'SessionSave' },
   keys = {
-    -- Will use Telescope if installed or a vim.ui.select picker otherwise
-    { '<leader>wr', '<cmd>SessionSearch<CR>', desc = 'Session search' },
+    { '<leader>wr', '<cmd>SessionRestore<CR>', desc = 'Session Restore' },
     { '<leader>ws', '<cmd>SessionSave<CR>', desc = 'Save session' },
-    { '<leader>wa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
   },
 
   ---enables autocomplete for opts
   ---@module "auto-session"
   ---@type AutoSession.Config
   opts = {
+    allowed_dirs = { '~/dev/*' },
+    auto_restore = false,
     -- The following are already the default values, no need to provide them if these are already the settings you want.
     session_lens = {
       mappings = {
@@ -22,29 +22,12 @@ return {
       },
 
       picker_opts = {
-        -- For Telescope, you can set theme options here, see:
-        -- https://github.com/nvim-telescope/telescope.nvim/blob/master/doc/telescope.txt#L112
-        -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/themes.lua
-        --
         border = true,
         layout_config = {
           width = 0.8, -- Can set width and height as percent of window
           height = 0.5,
         },
-
-        -- For Snacks, you can set layout options here, see:
-        -- https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#%EF%B8%8F-layouts
-        --
-        -- preset = "dropdown",
-        -- preview = false,
-        -- layout = {
-        --   width = 0.4,
-        --   height = 0.4,
-        -- },
       },
-
-      -- Telescope only: If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
-      load_on_setup = true,
     },
   },
 }
