a53fd25e509233083ccb8d6231067dd45ac3827e
lubasinkal <lubasinkal@outlook.com>
2025-07-18 17:21:01 +0200

    Telescope Update

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -5,18 +5,18 @@ return { -- Fuzzy Finder (files, lsp, etc)
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    -- { -- If encountering errors, see telescope-fzf-native README for installation instructions
-    --   'nvim-telescope/telescope-fzf-native.nvim',
-    --   build = 'make',
-    --   cond = function()
-    --     return vim.fn.executable 'make' == 1
-    --   end,
-    -- },
+    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
     { 'nvim-telescope/telescope-ui-select.nvim' },
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
     -- Uncomment the next line if you have a Nerd Font and want icons
-    -- { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
   config = function()
     -- [[ Configure Telescope ]]
