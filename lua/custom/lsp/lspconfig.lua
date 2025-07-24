cb57b1a1e2a344e177f7ae64677bfc8434ecb78f
lubasinkal <lubasinkal@outlook.com>
2025-07-24 17:45:41 +0200

    vue + vtsls update

--- lua/custom/lsp/lspconfig.lua
+++ lua/custom/lsp/lspconfig.lua
@@ -165,21 +165,10 @@ return {
       html = {},
       tailwindcss = {},
       cssls = {},
-
-      -- Vue 3 + TypeScript
-      vue_ls = {},
-      ts_ls = {
-        init_options = {
-          plugins = {
-            {
-              name = '@vue/typescript-plugin',
-              location = vim.fn.expand '$MASON/packages/vue-language-server/node_modules/@vue/language-server',
-              languages = { 'vue' },
-            },
-          },
-        },
-        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+      emmet_language_server = {
+        filetypes = { 'html', 'css', 'javascriptreact', 'typescriptreact', 'vue' },
       },
+      ts_ls = {},
       lua_ls = {},
       marksman = {}, -- Markdown LSP
     }
@@ -189,6 +178,8 @@ return {
       'stylua', -- Used to format Lua code
       'ruff',
       'biome',
+      'vue_ls',
+      'vtsls',
     })
 
     require('mason-tool-installer').setup {
@@ -215,5 +206,58 @@ return {
         end,
       },
     }
+    -- managed to get vue-language-server working with vtsls following https://github.com/vuejs/language-tools/wiki/Neovim
+    local vue_language_server_path = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server'
+
+    local vue_plugin = {
+      name = '@vue/typescript-plugin',
+      location = vue_language_server_path,
+      languages = { 'vue' },
+      configNamespace = 'typescript',
+    }
+    local vtsls_config = {
+      settings = {
+        vtsls = {
+          tsserver = {
+            globalPlugins = {
+              vue_plugin,
+            },
+          },
+        },
+      },
+      filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    }
+
+    local vue_ls_config = {
+      on_init = function(client)
+        client.handlers['tsserver/request'] = function(_, result, context)
+          local clients = vim.lsp.get_clients { bufnr = context.bufnr, name = 'vtsls' }
+          if #clients == 0 then
+            vim.notify('Could not found `vtsls` lsp client, vue_lsp would not work without it.', vim.log.levels.ERROR)
+            return
+          end
+          local ts_client = clients[1]
+
+          local param = unpack(result)
+          local id, command, payload = unpack(param)
+          ts_client:exec_cmd({
+            title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+            command = 'typescript.tsserverRequest',
+            arguments = {
+              command,
+              payload,
+            },
+          }, { bufnr = context.bufnr }, function(_, r)
+            local response_data = { { id, r.body } }
+            ---@diagnostic disable-next-line: param-type-mismatch
+            client:notify('tsserver/response', response_data)
+          end)
+        end
+      end,
+    }
+    -- nvim 0.11 or above
+    vim.lsp.config('vtsls', vtsls_config)
+    vim.lsp.config('vue_ls', vue_ls_config)
+    vim.lsp.enable { 'vtsls', 'vue_ls' }
   end,
 }
