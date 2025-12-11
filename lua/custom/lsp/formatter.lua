4ccd339df813aa27cb77b456bc0cb8ac6bfe60e6
lubasinkal <lubasinkal@outlook.com>
2025-12-11 20:44:52 +0200

    fix: Formatting and buffer visuals

--- lua/custom/lsp/formatter.lua
+++ lua/custom/lsp/formatter.lua
@@ -1,19 +1,27 @@
 return {
-  'stevearc/conform.nvim',
-  event = 'BufWritePost',
-  cmd = { 'ConformInfo', 'ConformFormat' },
-  opts = {
-    notify_on_error = true,
-    format_on_save = { async = true, lsp_fallback = true },
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      python = { 'ruff' },
-      javascript = { 'biome' },
-      typescript = { 'biome' },
-      vue = { 'biome' },
-      html = { 'biome' },
-      css = { 'biome' },
-      json = { 'biome' },
+    'stevearc/conform.nvim',
+    event = 'BufWritePost',
+    cmd = { 'ConformInfo', 'ConformFormat' },
+    opts = {
+        format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+        },
+        format_after_save = {
+            lsp_format = "fallback",
+        },
+        notify_on_error = true,
+        -- Conform will notify you when no formatters are available for the buffer
+        notify_no_formatters = true,
+        formatters_by_ft = {
+            python = { 'ruff' },
+            javascript = { 'biome' },
+            typescript = { 'biome' },
+            vue = { 'biome' },
+            html = { 'biome' },
+            css = { 'biome' },
+            json = { 'biome' },
+        },
     },
-  },
 }
--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,47 +1,46 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  keys = {
-    { '<Tab>', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next buffer' },
-    { '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Previous buffer' },
-  },
-  event = { 'BufReadPost', 'BufNewFile' },
-  config = function()
-    require('bufferline').setup {
-      options = {
-        mode = 'buffers',
-        themable = true,
-        numbers = 'none',
-        modified_icon = '●', -- Minimal dot for modified buffers
-        left_trunc_marker = ' ',
-        right_trunc_marker = ' ',
-        max_name_length = 20,
-        max_prefix_length = 15,
-        tab_size = 18,
-        diagnostics = 'nvim_lsp',
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+    },
+    keys = { { '<Tab>', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next buffer' },
+        { '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Previous buffer' },
+    },
+    event = { 'BufReadPost', 'BufNewFile' },
+    config = function()
+        require('bufferline').setup {
+            options = {
+                mode = 'buffers',
+                themable = true,
+                numbers = 'none',
+                modified_icon = '●', -- Minimal dot for modified buffers
+                left_trunc_marker = ' ',
+                right_trunc_marker = ' ',
+                max_name_length = 20,
+                max_prefix_length = 15,
+                tab_size = 18,
+                diagnostics = 'nvim_lsp',
 
-        diagnostics_indicator = function(count, level, diagnostics_dict, context)
-          local s = ' '
-          for e, n in pairs(diagnostics_dict) do
-            local sym = e == ' error' and '  ' or (e == ' warning' and '  ' or '  ')
-            s = s .. n .. sym
-          end
-          return s
-        end,
-        color_icons = true,
-        show_buffer_icons = true, -- Show filetype icons
-        separator_style = { '│', '│' },
-        enforce_regular_tabs = true,
-        always_show_bufferline = false,
-        auto_toggle_bufferline = true,
-        indicator = {
-          style = 'none', -- Options: 'icon', 'underline', 'none'
-        },
-        sort_by = 'insert_at_end',
-      },
-    }
-  end,
+                diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                    local s = ' '
+                    for e, n in pairs(diagnostics_dict) do
+                        local sym = e == ' error' and '  ' or (e == ' warning' and '  ' or '  ')
+                        s = s .. n .. sym
+                    end
+                    return s
+                end,
+                color_icons = true,
+                show_buffer_icons = true, -- Show filetype icons
+                separator_style = { '│', '│' },
+                enforce_regular_tabs = true,
+                always_show_bufferline = false,
+                auto_toggle_bufferline = true,
+                indicator = {
+                    style = 'none', -- Options: 'icon', 'underline', 'none'
+                },
+                sort_by = 'insert_at_end',
+            },
+        }
+    end,
 }
