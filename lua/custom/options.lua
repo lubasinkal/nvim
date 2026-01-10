a66139075e92d12973139516c3817c30bd94932b
lubasinkal <lubasinkal@outlook.com>
2026-01-10 21:25:53 +0200

    fix: Optimal options

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,63 +1,75 @@
--- init.lua
-vim.o.termguicolors = true
-vim.g.have_nerd_font = true -- set true if terminal font supports icons
+-- UI & General
+vim.g.have_nerd_font = true
 vim.opt.signcolumn = "yes"
-vim.opt.laststatus = 3 -- global statusline (saves space if you use one statusline plugin)
+vim.opt.laststatus = 3
 vim.opt.showtabline = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.numberwidth = 4
 vim.opt.cursorline = true
-vim.opt.scrolloff = 5
-vim.opt.sidescrolloff = 5
+vim.opt.scrolloff = 10 -- Increased: keeps cursor more centered
+vim.opt.sidescrolloff = 8
 vim.opt.wrap = false
 vim.opt.mouse = "a"
-vim.opt.fillchars:append({ eob = " ", diff = "╱" }) -- clean buffer tail + prettier diffs
-vim.opt.inccommand = "split"
+vim.opt.fillchars:append({ eob = " ", diff = "╱" })
+vim.opt.inccommand = "split" -- Great for previewing substitutions
 
+-- Performance & Files
 vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300 -- 200 is very fast; 300 is usually the "sweet spot" for chorded keys
 
--- Command height: modern Neovim lets you hide it
+-- Modern UI behavior
 vim.opt.cmdheight = 0
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e1e" })
 vim.opt.smoothscroll = true
-vim.opt.splitkeep = "screen"
-vim.opt.virtualedit = "block"
-vim.opt.smartindent = true
+vim.opt.splitkeep = "screen" -- Keeps text in place when opening splits
+vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+vim.opt.shortmess:append("IsC") -- Don't pass messages to |ins-completion-menu|
+
+-- Tabs & Indentation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.breakindent = true
-vim.opt.formatoptions:remove({ "c", "r", "o", "t" }) -- don't auto-insert comments or auto-wrap text
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
+vim.opt.formatoptions:remove({ "c", "r", "o" })
+
+-- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.whichwrap:append("<>[]hl")
-vim.opt.iskeyword:append("-")
+vim.opt.shada = "!,'100,<50,s10,h" -- Limit search history/registers to keep startup fast
+
+-- Navigation & Splits
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-vim.opt.undofile = true
-local undodir = vim.fn.stdpath("state") .. "/undo"
-if vim.fn.isdirectory(undodir) == 0 then
-	vim.fn.mkdir(undodir, "p")
-end
-vim.opt.undodir = undodir
-vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.whichwrap:append("<>[]hl")
+vim.opt.iskeyword:append("-") -- Treat dash-separated words as one unit (good for CSS/HTML)
+
+-- Completion & Wildmenu
+vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
 vim.opt.pumheight = 10
-vim.opt.conceallevel = 2 -- 2 = hide conceal chars (useful for JSON, markdown, LaTeX)
-vim.opt.concealcursor = "nc"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 200 -- reduced for faster response
-vim.api.nvim_create_autocmd("InsertEnter", {
-	command = "norm zz",
-})
--- Additional modern improvements
-vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
-vim.opt.grepformat = "%f:%l:%c:%m"
 vim.opt.wildmode = "longest:full,full"
 vim.opt.wildoptions = "pum"
-vim.opt.wildignorecase = true
+
+-- Conceal (Good for Obsidian/JSON/Markdown)
+vim.opt.conceallevel = 2
+vim.opt.concealcursor = "nc"
+
+-- System Integration
+vim.opt.clipboard = "unnamedplus"
+vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+vim.opt.grepformat = "%f:%l:%c:%m"
+
+-- Highlighting Fixes
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e1e" })
+
+-- Autocmds
+-- Auto-center on InsertEnter (Your preference)
+vim.api.nvim_create_autocmd("InsertEnter", {
+	callback = function()
+		vim.cmd("norm! zz")
+	end,
+})
