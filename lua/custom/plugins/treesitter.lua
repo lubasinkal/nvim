e04c3d460b9bd695a910466f350c5eb3d469cae9
lubasinkal <lubasinkal@outlook.com>
2026-01-12 19:08:59 +0200

    update treesitter

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,19 +1,18 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    event = { 'VeryLazy' },
-    cmd = { 'TSUpdate' },
-    main = 'nvim-treesitter.configs',
-
-    opts = {
-        -- Install languages that are not already installed above
-        auto_install = true,
-        indent = { enable = true },
-        highlight = { enable = true },
-        folds = { enable = true },
-        context_commentstring = {
-            enable = true,
-            enable_autocmd = true,
-        },
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  event = { 'VeryLazy' },
+  cmd = { 'TSUpdate' },
+  branch = 'master',
+  opts = {
+    -- Install languages that are not already installed above
+    auto_install = true,
+    indent = { enable = true },
+    highlight = { enable = true },
+    folds = { enable = true },
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = true,
     },
+  },
 }
