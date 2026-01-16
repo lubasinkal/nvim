684f26cda7df354d5b53d5e1f7d4040332a35666
lubasinkal <lubasinkal@outlook.com>
2026-01-16 21:09:48 +0200

    fix: Whichkey UI clean

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -2,7 +2,7 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
   opts = {
-    preset = 'classic', --classic,modern,helix
+    preset = 'helix', --classic,modern,helix
     -- delay between pressing a key and opening which-key (milliseconds)
     -- this setting is independent of vim.opt.timeoutlen
     icons = {
