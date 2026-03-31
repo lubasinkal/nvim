6db1712c1832f32e92b3b291071dcf355670282c
lubasinkal <lubasinkal@outlook.com>
2026-03-31 09:55:41 +0200

    new: from vague to catppuccin

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,15 +1,21 @@
 return {
-    "vague-theme/vague.nvim",
-    event = "VeryLazy",
-    config = function()
-        require("vague").setup({ transparent = true })
-        vim.cmd("colorscheme vague")
-
-        -- Defer statusline activation one more tick
-        vim.schedule(function()
-            require("custom.statusline").apply_highlights()
-            vim.o.statusline = "%!v:lua.statusline()"
-            vim.cmd("redrawstatus!")
-        end)
-    end,
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  event = 'VeryLazy',
+  config = function()
+    require('catppuccin').setup {
+      transparent_background = true,
+      auto_integrations = true,
+      float = {
+        transparent = true, -- enable transparent floating windows
+      },
+    }
+    vim.cmd.colorscheme 'catppuccin-nvim'
+    -- -- Defer statusline activation one more tick
+    -- vim.schedule(function()
+    --   require('custom.statusline').apply_highlights()
+    --   vim.o.statusline = '%!v:lua.statusline()'
+    --   vim.cmd 'redrawstatus!'
+    -- end)
+  end,
 }
