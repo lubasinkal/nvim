95a71ec50f9d9a52b55d4d47a65008065fb9511d
lubasinkal <lubasinkal@outlook.com>
2026-04-14 10:13:34 +0200

    fix: treesitter redundancies

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,24 +1,5 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    event = 'BufReadPost',
-    cmd = { 'TSUpdate' },
-    branch = 'main',
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true },
-    },
-    config = function()
-        vim.api.nvim_create_autocmd('FileType', {
-            callback = function(args)
-                local buf = args.buf
-                local lang = vim.treesitter.language.get_lang(args.match)
-                if not lang then
-                    return
-                end
-                vim.treesitter.start(buf, lang)
-                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-            end,
-        })
-    end,
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  event = 'BufReadPost',
 }
