9ca05c408c7d941d5e826bc06f5574d3ac295e74
lubasinkal <lubasinkal@outlook.com>
2026-05-03 17:43:33 +0200

    fix: nvim 0.12 mini incremental search

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -13,19 +13,26 @@ return {
         }
         require('mini.indentscope').setup()
         require('mini.pairs').setup()
-        require('mini.ai').setup { n_lines = 500 }
+        require('mini.ai').setup {
+            -- NOTE: Avoid conflicts with the built-in incremental selection mappings on Neovim>=0.12 (see `:help treesitter-incremental-selection`)
+            mappings = {
+                around_next = 'aa',
+                inside_next = 'ii',
+            },
+            n_lines = 500,
+        }
         require('mini.surround').setup {
             mappings = {
                 add = 'gsa', -- Add surrounding in Normal and Visual modes
                 --e.g. gsaiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
                 --e.g. gsaa}) - [S]urround [A]dd [A]round [}]Braces [)]Paren
-                delete = 'gsd', -- Delete surrounding
+                delete = 'gsd',         -- Delete surrounding
                 --e.g.    gsd"   - [S]urround [D]elete ["]quotes
-                replace = 'gsr', -- Replace surrounding
+                replace = 'gsr',        -- Replace surrounding
                 --e.g.     gsr)'  - [S]urround [R]eplace [)]Paren by [']quote
-                find = 'gsf',   -- Find surrounding (to the right)
-                find_left = 'gsF', -- Find surrounding (to the left)
-                highlight = 'gsh', -- Highlight surrounding
+                find = 'gsf',           -- Find surrounding (to the right)
+                find_left = 'gsF',      -- Find surrounding (to the left)
+                highlight = 'gsh',      -- Highlight surrounding
                 update_n_lines = 'gsn', -- Update `n_lines`
             },
             n_lines = 500,
