f44a0fdb99fee7cbfb8701ad8e5643aff2c0432c
lubasinkal <lubasinkal@outlook.com>
2026-05-06 15:55:12 +0200

    fix: brought back completions keymaps

--- lua/custom/lsp/autocomplete.lua
+++ lua/custom/lsp/autocomplete.lua
@@ -22,6 +22,42 @@ return {
     opts = {
         keymap = {
             preset = 'default',
+            ['<C-k>'] = { 'select_next', 'fallback' },
+            ['<C-j>'] = { 'select_prev', 'fallback' },
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+            -- prefer explicit accept key (C-y) to reduce accidental accepts with <CR>
+            ['<C-y>'] = { 'accept', 'fallback' },
+            ['<CR>'] = { 'accept', 'fallback' },
+            ['<C-e>'] = { 'cancel', 'fallback' },
+            ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            -- Tab / S-Tab: sensible fallbacks, snippet support, complete when appropriate
+            ['<Tab>'] = {
+                function(cmp)
+                    -- If popup visible, select next
+                    if cmp.is_visible() then
+                        return cmp.select_next()
+                    end
+                    -- If snippet engine has jump available, jump
+                    if cmp.snippet_active() then
+                        return cmp.snippet_forward()
+                    end
+                end,
+                'fallback',
+            },
+
+            ['<S-Tab>'] = {
+                function(cmp)
+                    if cmp.is_visible() then
+                        return cmp.select_prev()
+                    end
+                    if cmp.snippet_active() then
+                        return cmp.snippet_backward()
+                    end
+                    return cmp.fallback()
+                end,
+                'fallback',
+            },
         },
         appearance = {
             nerd_font_variant = 'mono',
