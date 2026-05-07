8af1dea0c25491fe59332d2400c43f204e0894d8
lubasinkal <lubasinkal@outlook.com>
2026-05-07 10:44:01 +0200

    fix: I've decided to return to Cyberdream
    Honestly I enjoy the high contrast

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,21 +1,13 @@
 return {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    "scottmckendry/cyberdream.nvim",
+    name = 'cyberdream',
     event = 'VeryLazy',
     config = function()
-        require('catppuccin').setup {
-            transparent_background = true,
-            auto_integrations = true,
-            float = {
-                transparent = true, -- enable transparent floating windows
-            },
-        }
-        vim.cmd.colorscheme 'catppuccin-nvim'
-        -- -- Defer statusline activation one more tick
-        -- vim.schedule(function()
-        --   require('config.util.statusline').apply_highlights()
-        --   vim.o.statusline = '%!v:lua.statusline()'
-        --   vim.cmd 'redrawstatus!'
-        -- end)
+        require("cyberdream").setup({
+            transparent = true,
+            italic_comments = true,
+            terminal_colors = true,
+        })
+        vim.cmd("colorscheme cyberdream")
     end,
 }
