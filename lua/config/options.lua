9ae73074aa80736cf59ec006461d314b51545c7e
lubasinkal <lubasinkal@outlook.com>
2026-05-25 17:16:15 +0200

    clean up options.lua: remove redundant defaults and comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,34 +1,24 @@
--- UI & General
 vim.g.have_nerd_font = true
-vim.opt.termguicolors = true
+
 vim.opt.signcolumn = 'yes'
-vim.opt.laststatus = 3
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.numberwidth = 4
-vim.opt.scrolloff = 10 -- Increased: keeps cursor more centered
+vim.opt.scrolloff = 10
 vim.opt.smoothscroll = true
 vim.opt.sidescrolloff = 8
-vim.opt.wrap = false
--- mouse defaults to 'nvi' in Neovim 0.10+
 vim.opt.fillchars:append { eob = ' ', diff = '╱' }
-vim.opt.inccommand = 'split' -- Great for previewing substitutions
+vim.opt.inccommand = 'split'
 vim.o.winborder = 'rounded'
--- Performance & Files
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
-vim.opt.updatetime = 200 -- faster CursorHold
-vim.opt.timeoutlen = 300 -- faster which-key
-vim.opt.shellcmdflag = "-c"
-vim.opt.shellquote = ""
-vim.opt.shellxquote = ""
+vim.opt.updatetime = 200
+vim.opt.timeoutlen = 300
+vim.opt.splitkeep = 'screen'
+vim.opt.virtualedit = 'block'
 
--- vim.opt.cmdheight = 0
-vim.opt.splitkeep = 'screen'  -- Keeps text in place when opening splits
-vim.opt.virtualedit = 'block' -- Allow cursor to move where there is no text in visual block mode
-
--- Tabs & Indentation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
@@ -36,18 +26,13 @@ vim.opt.expandtab = true
 vim.opt.breakindent = true
 vim.opt.formatoptions:remove { 'c', 'r', 'o' }
 
--- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.shada = "!,'100,<50,s10,h" -- Limit search history/registers to keep startup fast
+vim.opt.shada = "!,'100,<50,s10,h"
 
--- Navigation & Splits
--- splitright = true -- default in 0.10+
-vim.opt.splitbelow = true
 vim.opt.whichwrap:append '<>[]hl'
-vim.opt.iskeyword:append '-' -- Treat dash-separated words as one unit (good for CSS/HTML)
+vim.opt.iskeyword:append '-'
 
--- Completion & Wildmenu
 vim.o.pumborder = 'rounded'
 vim.o.pummaxwidth = 40
 vim.o.completeopt = 'menu,menuone,noselect,nearest'
@@ -55,18 +40,15 @@ vim.opt.pumheight = 10
 vim.opt.wildmode = 'longest:full,full'
 vim.opt.wildoptions = 'pum'
 
--- Conceal (Good for Obsidian/JSON/Markdown)
 vim.opt.conceallevel = 2
 vim.opt.concealcursor = 'nc'
 
--- System Integration
 vim.schedule(function()
     vim.o.clipboard = 'unnamedplus'
 end)
 vim.opt.grepprg = 'rg --vimgrep --no-heading --smart-case'
 vim.opt.grepformat = '%f:%l:%c:%m'
 
--- Highlighting Fixes
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#1e1e1e' })
 
@@ -75,22 +57,13 @@ vim.diagnostic.config({
     severity_sort = true,
     float = { border = 'rounded', source = 'if_many', header = '' },
     underline = { severity = { min = vim.diagnostic.severity.WARN } },
-
-    -- Can switch between these as you prefer
-    virtual_text = true,   -- Text shows up at the end of the line
-    virtual_lines = false, -- Text shows up underneath the line
-
-    -- Auto-open float when jumping with [d and ]d
+    virtual_text = true,
+    virtual_lines = false,
     jump = {
-        on_jump = function(diagnostic, bufnr)
-            if not diagnostic then
-                return
+        on_jump = function(diag, bufnr)
+            if diag then
+                vim.diagnostic.open_float { bufnr = bufnr, scope = 'cursor', focus = false }
             end
-            vim.diagnostic.open_float({
-                bufnr = bufnr,
-                scope = "cursor", -- or "line"
-                focus = false,
-            })
         end,
     },
 })
