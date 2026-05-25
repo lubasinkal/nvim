5c55660dc0424aa92aa0cce9bd41ba2ea4eeea0f
lubasinkal <lubasinkal@outlook.com>
2026-05-25 17:10:11 +0200

    move autocmds from options.lua to autocmd.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -70,44 +70,6 @@ vim.opt.grepformat = '%f:%l:%c:%m'
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#1e1e1e' })
 
--- Autocmds
--- Auto-center on InsertEnter (Your preference)
-vim.api.nvim_create_autocmd('InsertEnter', {
-    callback = function()
-        vim.cmd 'norm! zz'
-    end,
-})
--- Automatic window resizing
-vim.api.nvim_create_autocmd({ 'VimResized' }, {
-    callback = function()
-        vim.cmd 'redraw!'
-        vim.cmd 'wincmd ='
-    end,
-})
-
--- Vertical Help Page
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'help',
-    command = 'wincmd L',
-})
-
--- enable cursorline only in the active/current buffer/window and disable it when you leave
-vim.api.nvim_create_augroup('CursorLineActive', { clear = true })
-
-vim.api.nvim_create_autocmd({ 'WinEnter', 'BufWinEnter' }, {
-    group = 'CursorLineActive',
-    callback = function()
-        vim.wo.cursorline = true
-    end,
-})
-
-vim.api.nvim_create_autocmd('WinLeave', {
-    group = 'CursorLineActive',
-    callback = function()
-        vim.wo.cursorline = false
-    end,
-})
-
 vim.diagnostic.config({
     update_in_insert = false,
     severity_sort = true,
@@ -131,27 +93,4 @@ vim.diagnostic.config({
             })
         end,
     },
-})
-
--- Diagnonstic floating window on cursorhold
-vim.api.nvim_create_autocmd('CursorHold', {
-    callback = function()
-        if next(vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })) then
-            vim.diagnostic.open_float(nil, { focusable = false, border = 'rounded', scope = 'cursor' })
-        end
-    end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    callback = function()
-        local filetype = vim.bo.filetype
-
-        if filetype and filetype ~= "" then
-            pcall(vim.treesitter.start)
-            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-            vim.bo.indentexpr = "v:lua.vim.treesitter.indentexpr()"
-        end
-    end,
-
 })
