f0e5b68d22587301779c065b6366370a56edf5a6
lubasiverse <admin@lubasiverse.com>
2026-08-12 06:13:32 +0200

    fix(lsp): restore ts_ls Vue typescript-plugin passthrough

--- lua/config/lsp/lspconfig.lua
+++ lua/config/lsp/lspconfig.lua
@@ -61,6 +61,19 @@ vim.lsp.config('lua_ls', {
     },
   },
 })
+vim.lsp.config('ts_ls', {
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  init_options = {
+    plugins = {
+      {
+        name = '@vue/typescript-plugin',
+        location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+        languages = { 'vue' },
+        configNamespace = 'typescript',
+      },
+    },
+  },
+})
 require('mason').setup {
   ensure_installed = { 'stylua' },
 }
