0bd240d0db60a1132b6d6906e3b1badf68fc37b2
lubasiverse <admin@lubasiverse.com>
2026-08-13 14:10:59 +0200

    fix: rm friendly snippet to mini.snippet

--- lua/config/lsp/autocomplete.lua
+++ lua/config/lsp/autocomplete.lua
@@ -1,12 +1,16 @@
 vim.pack.add {
-  'https://github.com/rafamadriz/friendly-snippets',
   'https://github.com/roobert/tailwindcss-colorizer-cmp.nvim',
   { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range '1.*' },
 }
 require('tailwindcss-colorizer-cmp').setup { color_square_width = 2 }
 require('blink.cmp').setup {
+  -- use mini.snippets for expanding/navigating snippets (see :h blink.cmp-snippets-preset)
+  snippets = { preset = 'mini_snippets' },
   keymap = {
     preset = 'default',
+    -- super-tab style: jump snippet placeholders when active, else select in menu
+    ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+    ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
     ['<C-k>'] = { 'select_next', 'fallback' },
     ['<C-j>'] = { 'select_prev', 'fallback' },
     -- prefer explicit accept key (C-y) to reduce accidental accepts with <CR>
--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -9,6 +9,7 @@ require('mini.notify').setup {
 }
 require('mini.indentscope').setup()
 require('mini.pairs').setup()
+require('mini.snippets').setup()
 require('mini.ai').setup {
   mappings = {
     around_next = 'aa',
